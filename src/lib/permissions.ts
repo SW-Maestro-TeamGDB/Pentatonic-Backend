1de22a683702b3043e7589e601ba50c8299f7f44
SeungWon <20sunrin041@sunrint.hs.kr>
2021-06-28 14:20:54 +0900

    refactor: sms lib

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -1,9 +1,9 @@
 import { rule, shield } from "graphql-shield"
 import { ApolloError } from "apollo-server-express"
 
-const isValid = rule()((parent: void, args: void) => {
-    return true
-})
+// const isValid = rule()((parent: void, args: void) => {
+//     return true
+// })
 
 export const permissions = shield({
 
--- src/lib/sms.ts
+++ src/lib/sms.ts
@@ -20,35 +20,27 @@ const rand = (min: number, max: number) => Math.floor(Math.random() * (max - min
 export const smsRequest = async (smsNumber: string) => {
     const randNumber = rand(100000, 999999)
     const timeStamp = Date.now().toString()
-    try {
-        const result = await fetch(`https://sens.apigw.ntruss.com/sms/v2/services/${env.SMS_KEY}/messages`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-                "x-ncp-iam-access-key": env.NCP_ACCESS_KEY as string,
-                "x-ncp-apigw-timestamp": timeStamp,
-                "x-ncp-apigw-signature-v2": makeSignature(timeStamp)
-            },
-            body: JSON.stringify({
-                type: "SMS",
-                countryCode: "82",
-                from: env.PHONE_NUMBER as string,
-                contentType: "COMM",
-                content: `[Pentatonic] 본인확인 인증번호 \n[${randNumber}]를 화면에 입력해주세요`,
-                messages: [{
-                    to: smsNumber
-                }]
-            })
+    const result = await fetch(`https://sens.apigw.ntruss.com/sms/v2/services/${env.SMS_KEY}/messages`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "x-ncp-iam-access-key": env.NCP_ACCESS_KEY as string,
+            "x-ncp-apigw-timestamp": timeStamp,
+            "x-ncp-apigw-signature-v2": makeSignature(timeStamp)
+        },
+        body: JSON.stringify({
+            type: "SMS",
+            countryCode: "82",
+            from: env.PHONE_NUMBER as string,
+            contentType: "COMM",
+            content: `[Pentatonic] 본인확인 인증번호 \n[${randNumber}]를 화면에 입력해주세요`,
+            messages: [{
+                to: smsNumber
+            }]
         })
-        const resultJson = await result.json()
-        if (resultJson.statusName === "success") {
-            return randNumber
-        }
-        return false
-    } catch (e) {
-        console.log(e)
-        return false
-    }
+    })
+    const resultJson = await result.json()
+    return resultJson.statusName === "success" ? randNumber : false
 }
 
 export const changePhoneNumber = (phoneNumber: string) => `010${phoneNumber.slice(5, phoneNumber.length)}`
