c5b7caee5d4f028ffb4412fd66ed024962f82d1d
SeungWon <20sunrin041@sunrint.hs.kr>
2021-07-04 14:30:00 +0900

    feat: setup ci/cd

---
+++ deploy.ts
@@ -0,0 +1,17 @@
+import env from "dotenv"
+env.config()
+
+import { NodeSSH } from "node-ssh"
+
+const ssh = new NodeSSH()
+
+const deploy = async () => {
+    await ssh.connect({
+        host: process.env.HOST,
+        username: process.env.ID,
+        password: process.env.PW
+    })
+    await ssh.execCommand(`./deploy.sh`)
+    process.exit(0)
+}
+deploy()
