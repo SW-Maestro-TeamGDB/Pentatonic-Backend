9aba5aded60459b9b1433578777be54e6f5247a5
SeungWon <20sunrin041@sunrint.hs.kr>
2021-07-12 21:24:17 +0900

    feat: Add audio s3 upload logic

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,5 +1,6 @@
 import AWS from "aws-sdk"
 import { ReadStream } from "fs-capacitor"
+import { createReadStream } from "fs"
 import env from "config/env"
 
 const region = env.AWS_REGION as string
@@ -10,11 +11,11 @@ const S3 = new AWS.S3({
     secretAccessKey: env.AWS_PW as string
 })
 const Bucket = env.AWS_BUCKET as string
-export const uploadS3 = async (fileStream: ReadStream, Key: string, ContentType: string) => {
+export const uploadS3 = async (fileStream: ReadStream | string, Key: string, ContentType: string) => {
     const params = {
         Bucket,
         Key,
-        Body: fileStream,
+        Body: typeof fileStream === "string" ? createReadStream(fileStream) : fileStream,
         ACL: "public-read",
         ContentType
     }
