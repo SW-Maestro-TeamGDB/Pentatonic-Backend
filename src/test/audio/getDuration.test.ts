2ed68acde3100a494342cc257c1809734be8a38c
SeungWon <20sunrin041@sunrint.hs.kr>
2021-07-23 18:52:46 +0900

    fix: update mocha default timeout

--- src/test/audio/getDuration.test.ts
+++ src/test/audio/getDuration.test.ts
@@ -6,14 +6,14 @@ import env from "config/env"
 describe("getAudioDuration unit test", () => {
     it("get duration of the m4a file", async () => {
         const audioDuration = await getAudioDuration(`${env.S3_URI}/violin.m4a`)
-        equal(audioDuration, 216.711837)
+        equal(216 <= audioDuration && audioDuration <= 217, true)
     }).timeout(100000)
     it("get duration of the wav", async () => {
         const audioDuration = await getAudioDuration(`${env.S3_URI}/mr-1.wav`)
-        equal(audioDuration, 216.711837)
+        equal(216 <= audioDuration && audioDuration <= 217, true)
     }).timeout(100000)
     it("get duration of the mp3", async () => {
         const audioDuration = await getAudioDuration(`${env.S3_URI}/mr-2.mp3`)
-        equal(audioDuration, 216.737959)
+        equal(216 <= audioDuration && audioDuration <= 217, true)
     }).timeout(100000)
 }).timeout(100000 << 1)
