6f97aea558f6fc8edc0ea540c0d70e03cc41f789
SeungWon <20sunrin041@sunrint.hs.kr>
2021-08-11 01:44:28 +0900

    feat: Add Name scalar type

--- src/config/scalars.ts
+++ src/config/scalars.ts
@@ -2,6 +2,18 @@ import { UserInputError, ApolloError } from "apollo-server-errors"
 import { GraphQLScalarType, Kind } from "graphql"
 import Hangul from "hangul-js"
 
+const isValidName = async (value: string) => {
+    if (!(2 <= value.length && value.length <= 20)) {
+        throw new UserInputError("Name의 길이는 2이상 20이하여야 합니다")
+    }
+    for (const c of value) {
+        if (!('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z' || '0' <= c && c <= '9' || Hangul.isComplete(c) || specialCharacters.includes(c) === true)) {
+            throw new UserInputError("Name의 영문 소문자, 대문자, 숫자, 특수문자, 올바른 한글 이여야 합니다")
+        }
+    }
+    return value
+}
+
 const isValidId = (value: string) => {
     if (!(5 < value.length && value.length < 15)) {
         throw new UserInputError("id의 길이는 6이상 14이하여야 합니다")
@@ -79,8 +91,22 @@ const GraphQLUsername = new GraphQLScalarType({
     }
 })
 
+const GraphQLName = new GraphQLScalarType({
+    name: 'Name',
+    description: '길이는 2 ~ 20, 영문, 한글, 숫자, 특수문자',
+    parseValue: isValidName,
+    serialize: isValidName,
+    parseLiteral(ast) {
+        if (ast.kind === Kind.STRING) {
+            return isValidName(ast.value)
+        }
+        throw new UserInputError("Name은 String입니다")
+    }
+})
+
 export const customScalar = {
     Username: GraphQLUsername,
     Password: GraphQLPassword,
     Id: GraphQLId,
+    Name: GraphQLName
 }
