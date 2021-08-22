882d931c7106f21b9c62353374da02c39560c044
SeungWon <20sunrin041@sunrint.hs.kr>
2021-08-22 16:48:05 +0900

    feat: Add FreeSong Mutation

--- src/resolvers/app/freeBand/Mutation.ts
+++ src/resolvers/app/freeBand/Mutation.ts
@@ -1,6 +1,14 @@
-import { CreateFreeBandInput } from "resolvers/app/freeBand/models"
+import {
+    CreateFreeBandInput
+} from "resolvers/app/freeBand/models"
+import {
+    SessionInformation
+} from "resolvers/app/band/models"
 import { Context } from "config/types"
-import { getAudioDuration } from "lib"
+import {
+    getAudioDuration,
+    sessionParse
+} from "lib"
 import { ApolloError } from "apollo-server-express"
 export const createFreeBand = async (parent: void, args: CreateFreeBandInput, context: Context) => {
     const { name, artist, songURI } = args.input.song
@@ -8,11 +16,21 @@ export const createFreeBand = async (parent: void, args: CreateFreeBandInput, co
     if (duration === 0) {
         throw new ApolloError("음원 파일을 정상적으로 읽지 못했습니다")
     }
-    return context.db.collection("song").insertOne({
+    const sessionArr: SessionInformation = sessionParse(args.input.sessionConfig)
+    const song = await context.db.collection("song").insertOne({
         name,
         artist,
         songURI: songURI.href,
         isFreeSong: true,
         duration
+    }).then(({ ops }) => ops[0])
+    return context.db.collection("freeBand").insertOne({
+        songId: song._id,
+        creatorId: context.user.id,
+        name: args.input.band.name,
+        introduce: args.input.band.introduce,
+        backGroundURI: args.input.band.backGroundURI.href,
+        sessions: sessionArr,
+        createDate: new Date()
     }).then(({ ops }) => ops[0])
 }
