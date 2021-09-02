151a3f202f3f36d20adb944da97aebb18bf8596e
SeungWon <20sunrin041@sunrint.hs.kr>
2021-09-03 02:39:44 +0900

    chore: change getRankedUser query

--- src/resolvers/app/follow/Query.ts
+++ src/resolvers/app/follow/Query.ts
@@ -22,3 +22,24 @@ export const getFollowingList = async (parnet: void, args: GetFollowingListInput
         id: { $in: userIds }
     }).toArray()
 }
+
+export const getRankedUser = async (parent: void, args: void, context: Context) => {
+    const followCounts = await context.db.collection("follow").aggregate([
+        {
+            $group: {
+                _id: "$following",
+                count: { $sum: 1 }
+            }
+        },
+        {
+            $sort: { count: -1 }
+        }
+    ]).limit(100).toArray()
+    const userIds = followCounts.map(({ _id }) => _id)
+    const users = await context.db.collection("user").find({ id: { $in: userIds } }).toArray()
+    const mp = followCounts.reduce((acc, cur, index) => {
+        acc[cur._id] = index
+        return acc
+    }, {})
+    return users.sort((a, b) => mp[a.id] - mp[b.id])
+}
